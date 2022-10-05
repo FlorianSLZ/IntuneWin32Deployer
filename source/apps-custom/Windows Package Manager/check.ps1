a9b1efcc408b031a9fe6ba118cb348ebb3bbcead
FlorianSLZ <38555854+FlorianSLZ@users.noreply.github.com>
2022-10-05 14:21:32 +0200

    winget

--- source/apps-custom/Windows Package Manager/check.ps1
+++ source/apps-custom/Windows Package Manager/check.ps1
@@ -1,3 +1,7 @@
+$PackageName = "WindowsPackageManager"
+$Package_winget = "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+$Path_local = "$ENV:Programfiles\_MEM"
+
 #Get WinGet Path (system)
 $ResolveWingetPath = Resolve-Path "$env:ProgramFiles\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe"
 if ($ResolveWingetPath) {
@@ -6,6 +10,9 @@ if ($ResolveWingetPath) {
     $Script:Winget = "$WingetPath\winget.exe"
 
     if($([System.Diagnostics.FileVersionInfo]::GetVersionInfo($Winget).FileVersion) -ge [System.Version]$ProgramVersion_minimum){
-        Write-Host "Found it!"
+        $ProgramVersion_current = Get-Content -Path "$Path_local\Validation\$PackageName"
+        if($ProgramVersion_current -eq $Package_winget){
+            Write-Host "Found it!"
+        }
     }
 }
--- source/apps-custom/Windows Package Manager/install.ps1
+++ source/apps-custom/Windows Package Manager/install.ps1
@@ -1,41 +1,12 @@
 ﻿$PackageName = "WindowsPackageManager"
 $Package_winget = "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
-$Package_VCLibs = "VCLibs140.appx"
 $URL_winget = "https://aka.ms/getwinget"
-$URL_VCLibs = 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx'
 
 $Path_local = "$Env:Programfiles\_MEM"
 Start-Transcript -Path "$Path_local\Log\$ProgramName-install.log" -Force
 
 # Force using TLS 1.2 connection
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
-##############################################################################
-#   Install VCLibs
-##############################################################################
-# Program/Installation folder
-$Folder_install = "$Path_local\Data\$Package_VCLibs"
-New-Item -Path $Folder_install -ItemType Directory -Force -Confirm:$false
-
-# Download current VCLibs MSIXBundle
-$wc = New-Object net.webclient
-$wc.Downloadfile($URL_winget, "$Folder_install\$Package_VCLibs")
-
-# Install VCLibs MSIXBundle 
-try{
-    Add-AppxPackage -Path "$Folder_install\$Package_VCLibs" 
-    Write-Host "Installation of $Package_VCLibs finished"
-}catch{
-    Write-Error "Failed to install $Package_VCLibs!"
-} 
-
-# Install file cleanup
-Start-Sleep 3 # to unblock installation file
-Remove-Item -Path "$Folder_install" -Force -Recurse
-
-# check installation of VCLibs
-if(!$(Get-AppxPackage -Name 'Microsoft.VCLibs.140.00.UWPDesktop' -ErrorAction SilentlyContinue)){exit 1}
-
 
 ##############################################################################
 #   Install winget
@@ -45,22 +16,18 @@ $Folder_install = "$Path_local\Data\$PackageName"
 New-Item -Path $Folder_install -ItemType Directory -Force -Confirm:$false
 
 # Download current winget MSIXBundle
-$wc = New-Object net.webclient
-$wc.Downloadfile($URL_winget, "$Folder_install\$Package_winget")
+Write-Host "Downloading stabel version of winget from: $($URL_winget)"
+$wc = New-Object System.Net.WebClient
+$wc.Downloadfile($URL_winget, "$env:Temp\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle")
 
 # Install WinGet MSIXBundle 
 try{
-    Add-AppxProvisionedPackage -Online -PackagePath "$Folder_install\$Package_winget" -SkipLicense
+    Add-AppxProvisionedPackage -Online -PackagePath "$env:Temp\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -SkipLicense
     Start-Sleep -Seconds 3
-    Add-AppxPackage -Path "$Folder_install\$Package_winget" -ForceUpdateFromAnyVersion -ForceApplicationShutdown
+    New-Item -Path "$Path_local\Validation\$PackageName" -ItemType "file" -Force -Value $Package_winget
     Write-Host "Installation of $PackageName finished"
 }catch{
     Write-Error "Failed to install $PackageName!"
 } 
-
-# Install file cleanup
-Start-Sleep 3 # to unblock installation file
-Remove-Item -Path "$Folder_install" -Force -Recurse
-
 
 Stop-Transcript

071673875dd918d3fbf837ff4004cd5e81b725ed
Florian Salzmann <38555854+FlorianSLZ@users.noreply.github.com>
2023-07-17 17:29:45 +0200

    choco v2.0

--- source/apps-custom/Chocolatey/install.ps1
+++ source/apps-custom/Chocolatey/install.ps1
@@ -8,7 +8,7 @@ try{
         Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
     }
 
-    C:\ProgramData\chocolatey\choco.exe list -lo
+    C:\ProgramData\chocolatey\choco.exe list
 
     choco feature enable -n=useRememberedArgumentsForUpgrades
     

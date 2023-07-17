405648af5abd46e88808fb58fb8308bd28ecebbc
Florian Salzmann <reg@salzmann.pro>
2023-07-17 15:40:57 +0000

    choco v2.0

--- source/ressources/template/choco/install.ps1
+++ source/ressources/template/choco/install.ps1
@@ -2,7 +2,7 @@
 $Path_4Log = "$Env:Programfiles\_MEM"
 Start-Transcript -Path "$Path_4Log\Log\$ProgramName-install.log" -Force
 
-$localprograms = C:\ProgramData\chocolatey\choco.exe list --localonly
+$localprograms = C:\ProgramData\chocolatey\choco.exe list
 if ($localprograms -like "*$ProgramName*"){
     C:\ProgramData\chocolatey\choco.exe upgrade $ProgramName -y
 }else{
